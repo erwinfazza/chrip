395dade5b95fe5c3cccd918dc0f45be4e02e8855
erwin <erwinintifazza@gmail.com>
2022-11-29 08:41:08 +0700

    membuat main content

--- script.js
+++
@@ -1,6 +0,0 @@
-const toggleButton = document.getElementsByClassName("toggle-button")[0];
-const NavLink = document.getElementsByClassName("nav-link")[0];
-
-toggleButton.addEventListener("click", () => {
-  NavLink.classList.toggle("active");
-});
